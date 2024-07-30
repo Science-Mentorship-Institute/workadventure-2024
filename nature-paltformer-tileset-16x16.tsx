c54b4356d23c0ed90efb3f9a7012dfb5b0800354
Aashini S Shah <aashah28@MIT.EDU>
2024-07-30 12:42:14 -0400

    firstTryAS

---
+++ nature-paltformer-tileset-16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="nature-paltformer-tileset-16x16" tilewidth="32" tileheight="32" spacing="1" tilecount="15" columns="3">
+ <image source="Nature Platformer Tileset [16x16][FREE] - RottingPixels/nature-paltformer-tileset-16x16.png" trans="17ff78" width="112" height="176"/>
+</tileset>
