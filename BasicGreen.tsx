2567ad98bb9364f949abf7598c5d6d213f526f06
Aashini S Shah <aashah28@MIT.EDU>
2024-08-01 21:20:36 -0400

    aashini test

--- BasicGreen.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="BasicGreen" tilewidth="32" tileheight="32" spacing="1" tilecount="4" columns="4">
- <image source="Starter Tiles Platformer/BasicGreen.png" trans="0b0fff" width="160" height="64"/>
-</tileset>
